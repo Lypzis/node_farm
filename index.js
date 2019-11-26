43076b4af4187a94d88090e2f32caa29bad4b76f
Victor V. Piccoli <victorvp2008@hotmail.com>
2019-11-26 16:33:00 -0300

    Updating templates

--- index.js
+++ index.js
@@ -1,6 +1,121 @@
 const fs = require('fs'); // fs stands for 'file system'
+const http = require('http'); // a server
+const url = require('url');
 
+/**
+ * Examples of read and write files in Node
+ * - Blocking, synchronous way
+ 
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); 
+console.log(textIn); // returns the content written in the file(a .txt in this case)
 
-const hello = 'Hello world! :D';
+const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut); // will create a new file in the specified folder, with content of 'textOut'
+console.log('File written! :D');
+///////////////////////////////////////////////
+*/
 
-console.log(hello);
+/**
+ * - Non-Blocking, Asynchronous way
+   - callback hell...
+   
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => { // first error, then data, mostly always
+    if(err) return console.log(`Error!\n${err}`);
+    
+    // uses the data retrieved as part of the argument
+    fs.readFile(`./txt/${data}.txt`, 'utf-8', (err, data2) => { 
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => { 
+            console.log(data3);
+            
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written :D');
+            });
+        });
+    });
+}); // reads asynchronous
+console.log('Will read file!');
+///////////////////////////////////////////////////
+*/
+
+/**
+ * Server
+ */
+// top level code can be synch without problems, since will only execute one time 
+// it is also immutable, so it would be bad to be reading it over and over again for each time 
+// '/api' is accessed, so instead, its data is read once and saved in a constant
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data); // object from the data read from the json file.
+
+const replaceTemplate = (temp, product) => {
+    // use regex for replace
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image); // good practice
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    else output = output.replace(/{%NOT_ORGANIC%}/g, product.organic);
+
+    return output;
+}
+
+const server = http.createServer((req, res) => {
+    // each time the server receives a request,
+    // this callback funtion is fired;
+    //console.log(req);
+    //console.log(req.headers);
+    console.log(req.url);
+    //Routing
+    const pathName = req.url;
+
+    switch (pathName) {
+        case '/':
+            res.writeHead(200, {'Content-type': 'text/html'});
+
+            // creates cards with specif data
+            const cardsHtml  = dataObject.map(e => replaceTemplate(tempCard, e)).join(''); // join to transform the result into a string
+            
+            // then inserts them inside the overview template
+            const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+            res.end(output);
+            break;
+        case '/overview':
+            res.end('This is the overview');
+            break;
+        case '/product':
+            res.end('This is the product');
+            break;
+        case '/api':
+                res.writeHead(200, {'Content-type': 'application/json'}); // the type of content for the header
+                // The response will be our "API" sending our data :D
+                res.end(data);
+            break;
+        default:
+            res.writeHead(404, {
+                // header
+                'Content-type': 'text',
+                'my-own-header': 'hello-world'
+            });
+            res.end('<h1>Page not found!</h1>');
+    }
+
+    //res.end('Hello from the server!'); // sends a response from the server which by the time has received a request :D
+}); // server has always request and response as arguments
+
+// the usual port for node is 8000, next is the host
+// this will be loop listening for resquests
+server.listen('8000', '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+}); // now just access localhost:8000 and check it out
+/////////////////////////////////////////////////
+
