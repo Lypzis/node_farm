cc979a4c8cb1bca65c98f029057ae2e7b4134547
Lypzis <victorvp2008@hotmail.com>
2019-11-28 15:07:55 +0000

    Adding prettier and vscode config

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require('fs'); // fs stands for 'file system'
 const http = require('http'); // a server
 const url = require('url');
+const slugify = require('slugify'); // "beautifies" urls
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
 /**
+ * FILES
  * Examples of read and write files in Node
  * - Blocking, synchronous way
  
@@ -41,10 +43,10 @@ console.log('Will read file!');
 */
 
 /**
- * Server
+ * SERVER
  */
-// top level code can be synch without problems, since will only execute one time 
-// it is also immutable, so it would be bad to be reading it over and over again for each time 
+// top level code can be synch without problems, since will only execute one time
+// it is also immutable, so it would be bad to be reading it over and over again for each time
 // '/api' is accessed, so instead, its data is read once and saved in a constant
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -53,60 +55,64 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data); // object from the data read from the json file.
 
+const slugs = dataObject.map(e => slugify(e.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-    // each time the server receives a request,
-    // this callback funtion is fired;
-    //console.log(req);
-    //console.log(req.headers);
-    //console.log(req.url);
+  // each time the server receives a request,
+  // this callback funtion is fired;
+  //console.log(req);
+  //console.log(req.headers);
+  //console.log(req.url);
 
-    // this object is destructured, these are two constants
-    const {query, pathname} = url.parse(req.url, true);
-    let output;
+  // this object is destructured, these are two constants
+  const { query, pathname } = url.parse(req.url, true);
+  let output;
 
-    switch (pathname) {
-        case '/':
-            res.writeHead(200, {'Content-type': 'text/html'});
+  switch (pathname) {
+    case '/':
+      res.writeHead(200, { 'Content-type': 'text/html' });
 
-            // creates cards with specif data
-            const cardsHtml  = dataObject.map(e => replaceTemplate(tempCard, e)).join(''); // join to transform the result into a string
-            
-            // then inserts them inside the overview template
-            output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+      // creates cards with specif data
+      const cardsHtml = dataObject.map(e => replaceTemplate(tempCard, e)).join(''); // join to transform the result into a string
 
-            res.end(output);
-            break;
-        case '/overview':
-            res.end('This is the overview');
-            break;
-        case '/product':
-            res.writeHead(200, {'Content-type': 'text/html'});
-            const product = dataObject[query.id]; // the selected product from the query of the respective id
-            output = replaceTemplate(tempProduct, product);
-            
-            res.end(output);
-            break;
-        case '/api':
-                res.writeHead(200, {'Content-type': 'application/json'}); // the type of content for the header
-                // The response will be our "API" sending our data :D
-                res.end(data);
-            break;
-        default:
-            res.writeHead(404, {
-                // header
-                'Content-type': 'text',
-                'my-own-header': 'hello-world'
-            });
-            res.end('<h1>Page not found!</h1>');
-    }
+      // then inserts them inside the overview template
+      output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
 
-    //res.end('Hello from the server!'); // sends a response from the server which by the time has received a request :D
+      res.end(output);
+      break;
+    case '/overview':
+      res.end('This is the overview');
+      break;
+    case '/product':
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      const product = dataObject[query.id]; // the selected product from the query of the respective id
+      output = replaceTemplate(tempProduct, product);
+
+      console.log(output);
+
+      res.end(output);
+      break;
+    case '/api':
+      res.writeHead(200, { 'Content-type': 'application/json' }); // the type of content for the header
+      // The response will be our "API" sending our data :D
+      res.end(data);
+      break;
+    default:
+      res.writeHead(404, {
+        // header
+        'Content-type': 'text',
+        'my-own-header': 'hello-world'
+      });
+      res.end('<h1>Page not found!</h1>');
+  }
+
+  //res.end('Hello from the server!'); // sends a response from the server which by the time has received a request :D
 }); // server has always request and response as arguments
 
 // the usual port for node is 8000, next is the host
 // this will be loop listening for resquests
 server.listen('8000', '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port 8000');
 }); // now just access localhost:8000 and check it out
 /////////////////////////////////////////////////
-
